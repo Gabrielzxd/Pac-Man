1822ec56970699027ebb30818c508a411048016e
Gabrielzxd <gabbrielssilva.biel@gmail.com>
2025-09-27 20:25:41 -0400

    Melhora no Maze.java

--- Maze.java
+++ Maze.java
@@ -5,24 +5,52 @@ public class Maze{
     private int[][] map = new int[32][28];
 
     public boolean isWall(int x, int y){
-
+        if (map[x][y] == -1){
+            return true;
+        } else{
+            return false;
+        }
     }
+    // o código acima ele não é muito útil, porque o pacman não estará em cima da posição que é uma parede,
+    // então eu tenho que ver primeiro como vou fazer a movimentação para que eu possa fazer uma checagem mais inteligente em cima disso.
 
     public boolean haspellot(int x, int y){
-
+        if(map[x][y] == 1){
+            return true;
+        } else{
+            return false;
+        }
     }
 
     public void eatPellot(int x, int y){
-
+        if (map[x][y] == 1){
+            map[x][y] = 0;
+        }
     }
 
     public Maze(){
-        String layout = "/home/gabriel/Pac-Man/Maze";
+        String pathMaze = "/home/gabriel/Pac-Man/Maze";
         BufferedReader reader = null;
         try{
-            FileReader readerFile = new FileReader(layout);
-            reader = new BufferedReader();
-
+            FileReader readerFile = new FileReader(pathMaze);
+            reader = new BufferedReader(readerFile);
+            int i = 0;
+            String line = reader.readLine();
+            while (line != null){
+                for(int j = 0; j < line.length(); j++){
+                    if(line.charAt(j) == '#'){
+                        map[i][j] = -1;
+                    }
+                    if(line.charAt(j) == '.'){
+                        map[i][j] = 1;
+                    }
+                    if(line.charAt(j) == '-'){
+                        map[i][j] = 0;
+                    }
+                }
+                i = i + 1;
+                line = reader.readLine();
+            }
         } catch (Exception erro){
             System.out.println("Erro ao tentar criar o labirinto");
             System.out.println(erro.getMessage());
